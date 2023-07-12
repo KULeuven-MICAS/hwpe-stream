2f7377473206775f8ece1a66f204ecdea6190a6f
rgantonio <ryan.antonio@esat.kuleuven.be>
2023-07-12 11:42:44 +0200

    cocotb: Improve test_hwpe_stream_merge test with proper assertions

--- cocotb/basic/test_hwpe_stream_merge.py
+++ cocotb/basic/test_hwpe_stream_merge.py
@@ -11,6 +11,7 @@
 import os
 import yaml
 import random
+import math
 
 #-----------------------------------
 # Importing cocotb 
@@ -39,11 +40,22 @@ toplevel     = 'tb_hwpe_stream_merge'
 # Specify python test name that contains the @cocotb.test. Usually the name of this test.
 module       = "test_hwpe_stream_merge"
 # Specify what simulator to use (e.g., verilator, modelsim, icarus)
-simulator    = "modelsim"
+simulator    = "verilator"
 # Specify build directory
 sim_build    = basic_path + "/sim_build/{}/".format(toplevel)
 # Get YAML source files path
 src_yml_path = hwpe_stream_path + "/src_files.yml"
+
+#-----------------------------------
+# Global parameters for testing
+# TODO: These are modifiable so change whenever needed
+#-----------------------------------
+# Checker parameter
+CHECK_COUNT   = 5
+
+# DUT parameters
+NB_IN_STREAMS = 4
+DATA_WIDTH    = 16
 
 #-----------------------------------
 # Get YAML source
@@ -89,12 +101,9 @@ async def hwpe_stream_merge(dut):
     #-----------------------------------
     # Modifiable parameters
     #-----------------------------------
-    CHECK_COUNT   = 10              # Checker parameter
-    NB_IN_STREAMS = 2               # DUT parameter
-    DATA_WIDTH    = 8               # DUT parameter
-    STRB_WIDTH    = DATA_WIDTH / 8  # Don't touch this
-    MAX_DATA_VAL  = 2**DATA_WIDTH-1 # Don't touch this
-    MAX_STRB_VAL  = 2**STRB_WIDTH-1 # Don't touch this
+    STRB_WIDTH    = int(DATA_WIDTH / 8)  # Don't touch this
+    MAX_DATA_VAL  = 2**DATA_WIDTH-1      # Don't touch this
+    MAX_STRB_VAL  = 2**STRB_WIDTH-1      # Don't touch this
 
     # Check first DATA_WIDTH is multiple of 8
     assert ((DATA_WIDTH % 8) == 0), f"{DATA_WIDTH} is not a multiple of 8!"
@@ -151,53 +160,106 @@ async def hwpe_stream_merge(dut):
 
     for i in range(CHECK_COUNT):
 
-        for j in range(NB_IN_STREAMS):
+        # Initialize empty arrays
+        push_data  = [0]*NB_IN_STREAMS
+        push_valid = [0]*NB_IN_STREAMS
+        push_strb  = [0]*NB_IN_STREAMS
+        push_ready = [0]*NB_IN_STREAMS
 
-            push_data  = random.randint(0,MAX_DATA_VAL)
-            push_valid = random.randint(0,1)
-            push_strb  = int(MAX_STRB_VAL)
+        cocotb.log.info(f'------------------------------------ ITERATION # {i} ------------------------------------')
 
-            pop_ready  = random.randint(0,1)
+        # Stimuli setting for NB_IN_STREAMS inputs
+        cocotb.log.info(f'===== INPUT LOG =====')
 
-            dut. data_i[j].value  = push_data
-            dut.valid_i[j].value  = push_valid
-            dut. strb_i[j].value  = push_strb
-            dut.   ready_o.value  = pop_ready
+        for j in range(NB_IN_STREAMS-1,-1,-1):
 
-            cocotb.log.info(f'------------------------------------ INPUT LOG ------------------------------------')
-            cocotb.log.info(f'push_i[{j}].data.value  = {hex(push_data)}')
-            cocotb.log.info(f'push_i[{j}].valid.value = {    push_valid}')
-            cocotb.log.info(f'push_i[{j}].strb.value  = {     push_strb}')
-            cocotb.log.info(f'pop_o.ready.value     = {     pop_ready}')
+            # Get random data
+            push_data [j] = random.randint(0,MAX_DATA_VAL)
+            push_valid[j] = random.randint(0,1)
+            push_strb [j] = random.randint(0,MAX_STRB_VAL)
+            
+            # Push into the stimulus
+            dut. data_i[j].value  = push_data [j]
+            dut.valid_i[j].value  = push_valid[j]
+            dut. strb_i[j].value  = push_strb [j]
+
+            # Record log
+            cocotb.log.info(f'----- INPUT STREAM # {j} -----')
+            cocotb.log.info(f'push_i[{j}].data.value  = {hex(push_data[j])}')
+            cocotb.log.info(f'push_i[{j}].valid.value = {   push_valid[j] }')
+            cocotb.log.info(f'push_i[{j}].strb.value  = {bin(push_strb[j])}')
+            
+        # Not part of streams since this is a single value only
+        pop_ready  = random.randint(0,1)
+        dut.ready_o.value  = pop_ready
+        cocotb.log.info(f'----- OUTPUT READY -----')
+        cocotb.log.info(f'pop_o.ready.value     = {pop_ready}')
         
+        # Next time step for simulation to evaluate data
         await Timer(1, units="ns")
         
-        pop_valid =     dut.valid_o.value
-        pop_data  = hex(dut.data_o.value)
+        # Extract outputs
+        cocotb.log.info(f'===== OUTPUT LOG =====')
 
-        cocotb.log.info(f'------------------------------------ OUTPUT LOG ------------------------------------')
+        pop_valid = dut.valid_o.value
+        pop_data  = int(dut.data_o.value)
+        pop_strb  = int(dut.strb_o.value)
 
         for j in range(NB_IN_STREAMS):
-            push_ready = dut.ready_i.value
-            cocotb.log.info(f'push_i[{j}].ready.value = {push_ready}')
+            push_ready[j] = dut.ready_i[j].value
+            cocotb.log.info(f'push_i[{j}].ready.value = {push_ready[j]}')
 
         cocotb.log.info(f'pop_o.valid.value     = {pop_valid}')
-        cocotb.log.info(f'pop_o.data.value      = { pop_data}')
+        cocotb.log.info(f'pop_o.data.value      = {hex(pop_data)}')
+        cocotb.log.info(f'pop_o.strb.value      = {bin(pop_strb)}')
 
         #-----------------------------------
         # Assertion checks
         #-----------------------------------
-        
-        
 
+        # Checking for output mismatch
+        # Expected output should be a simple concatenation of inputs
+        # Note that MSB is higher number in array
+        data_check   = 0
+        strb_check  = 0
+        valid_check = 0
+        ready_flag  = 1
 
+        for j in range(NB_IN_STREAMS-1,-1,-1):
 
+            data_check   = (data_check << DATA_WIDTH) + push_data[j]
+            strb_check   = (strb_check << STRB_WIDTH) + push_strb[j]
+            valid_check += push_valid[j]
+
+            if(pop_ready != push_ready[j]):
+                ready_flag = 0
+
+        # Checking for data mismatch
+        assert(data_check == pop_data), f"ERROR! Output mismatch - Expected output: {hex(data_check)}; Actual output: {hex(pop_data)}"
+
+        # Checking for strb mismatch
+        assert(strb_check == pop_strb), f"ERROR! Output mismatch - Expected output: {bin(strb_check)}; Actual output: {bin(pop_strb)}"
+
+        # Checking for valid mismatch
+        valid_expect = math.floor(valid_check/NB_IN_STREAMS)
+        assert(valid_expect == pop_valid), f"ERROR! Valid mismatch - Expected valid: {valid_expect}; Actual output: {pop_valid}"
+
+        # Checking for ready mismatch
+        assert (ready_flag == 1), f"ERROR! Ready mismatch - Double check ready values. Output ready input should be the same for input ready signals."
 
 
 #-----------------------------------
 # Pytest run
 #-----------------------------------
-def test_hwpe_stream_merge():
+
+# Parametrization
+@pytest.mark.parametrize(
+"parameters", [
+    {"DATA_WIDTH": str(DATA_WIDTH), "NB_IN_STREAMS": str(NB_IN_STREAMS)}
+])
+
+# Main test run
+def test_hwpe_stream_merge(parameters):
 
     global rtl_sources
     global include_folders
@@ -207,12 +269,11 @@ def test_hwpe_stream_merge():
     global sim_build
     
     run(
-        includes        =include_folders,
-        verilog_sources =rtl_sources,
-        toplevel        =toplevel,   
-        module          =module,
-        simulator       =simulator,    
-        sim_build       =sim_build
-    )        
-
-
+        includes        = include_folders,
+        verilog_sources = rtl_sources,
+        toplevel        = toplevel,   
+        module          = module,
+        simulator       = simulator,    
+        sim_build       = sim_build,
+        parameters      = parameters
+    )
